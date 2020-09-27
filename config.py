b7b2260f38fe045a2dc99c5f5d7621ccdc259336
LucyLu <44778814+YuShenLu@users.noreply.github.com>
2020-09-27 02:59:06 -0400

    restructure (#19)

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ LOG_PATH : str
     log files folder
 """
 
-import os, inspect
+import os, inspect, shutil
 
 CWD = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 LOG_PATH = CWD + '/log_files/'
@@ -27,7 +27,24 @@ REWARD_CSV = LOG_PATH + 'reward_log.csv'
 PARAMETER_LOG = LOG_PATH + 'parameter_log.csv'
 Num_bus_in_rep = LOG_PATH + 'Num_bus_in_rep.txt'
 
+
+def clean_folder_and_initialize():
+    folder = LOG_PATH
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    os.makedirs(LOG_PATH, exist_ok=True)
+    with open(PARAMETER_LOG, 'w+') as log_file:
+        log_file.write('log, replication ID, vehicle ID, checkin time, checkout time, check in phase number, check in phase time, checkout phase time, checkin headway, checkout headway, action 1, action 2 as decided at the bus check in, registered action at bus check out, Travel time, reward, prePOZ bus checkout time, prePOZ numbus, last_available_checkout_time, last_check_in_time, check_in_hdy, numbus, allnumvel, tToNearGreenPhase,\n')
+
 INTERSECTION_1 = {
+    'corridor_log': PARAMETER_LOG,
     'intersection': 1171274,
     'busCallDetector': 1171405,
     'busExitDetector': 1171391,
@@ -44,6 +61,7 @@ INTERSECTION_1 = {
 
 }
 INTERSECTION_2 = {
+    'corridor_log': PARAMETER_LOG,
     'intersection': 1171288,
     'busCallDetector': 1171407,
     'busExitDetector': 1171389,
@@ -61,3 +79,4 @@ INTERSECTION_2 = {
 
 
 CORRIDOR = [INTERSECTION_1, INTERSECTION_2]
+
